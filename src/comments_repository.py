1f5875bd063c84c05ce978880a7c6b79b7644e69
lxnd <lxnd@protonmail.com>
2025-02-06 13:52:31 +0100

    update: implement messages on postgres

--- src/comments_repository.py
+++ src/comments_repository.py
@@ -3,6 +3,8 @@ import sqlite3
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
 
+import psycopg
+
 from src.domain import UnregisteredComment, Comment, Message
 
 
@@ -104,3 +106,70 @@ class SQLLiteCommentsRepository(CommentsRepository):
             ]
         return comments
 
+
+class PostgresCommentsRepository(CommentsRepository):
+    def __init__(self, connection: psycopg.Connection):
+        self._connection = connection
+        with connection.cursor() as cur:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS comments (
+                    id SERIAL,
+                    post_slug text,
+                    author_name text,
+                    author_email text,
+                    message text,
+                    created_on date DEFAULT CURRENT_DATE
+                )
+                """
+            )
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS messages (
+                    author_name text,
+                    author_email text,
+                    message text,
+                    created_on timestamp
+                )
+                """
+            )
+
+    def add_message(self, message: Message):
+        with self._connection.cursor() as cur:
+            cur.execute(
+                "INSERT INTO messages (author_name, author_email, message, created_on) VALUES (%s, %s, %s, %s)",
+                (message.author_name, message.author_email, message.message, message.date)
+            )
+
+    def messages(self) -> list[Message]:
+        with self._connection.cursor() as cur:
+            res = cur.execute("SELECT author_name, author_email, message, created_on FROM messages")
+            messages = [Message(author_name=r[0], author_email=r[1], message=r[2], date=r[3]) for r in res.fetchall()]
+            return messages
+
+
+    def add_comment(self, comment: UnregisteredComment):
+        with self._connection.cursor() as cur:
+            cur.execute(
+                "INSERT INTO comments (post_slug, author_name, author_email, message, created_on) values (%s, %s, %s, %s, %s)",
+                (comment.post_slug, comment.author_name, comment.author_email, comment.message, comment.date),
+            )
+
+    def comments(self, post_slug: str) -> list[Comment]:
+        with self._connection.cursor() as cur:
+            cur.execute(
+                "SELECT id, post_slug, author_name, author_email, message, created_on FROM comments where post_slug = %s",
+                (post_slug,),
+            )
+            results = list(cur.fetchall())
+            return [
+                Comment(
+                    comment_id=r[0],
+                    post_slug=r[1],
+                    author_name=r[2],
+                    author_email=r[3],
+                    message=r[4],
+                    date=r[5],
+                )
+                for r in results
+            ]
--- tests/test_postgres_comments_repository.py
+++ tests/test_postgres_comments_repository.py
@@ -1,8 +1,8 @@
-import datetime as dt
+from datetime import date, datetime
 
 import psycopg
 
-from src.comments_repository import CommentsRepository
+from src.comments_repository import PostgresCommentsRepository
 from src.domain import UnregisteredComment, Comment, Message
 
 
@@ -14,66 +14,6 @@ def get_test_db_connection():
             cur.execute("CREATE DATABASE petitapetitio_tests_db")
 
     return psycopg.connect("dbname=petitapetitio_tests_db")  ## user=petitapetitio_tests_user")
-
-
-class PostgresCommentsRepository(CommentsRepository):
-    def __init__(self, connection: psycopg.Connection):
-        self._connection = connection
-        with connection.cursor() as cur:
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS comments (
-                    id SERIAL,
-                    post_slug text,
-                    author_name text,
-                    author_email text,
-                    message text,
-                    created_on date DEFAULT CURRENT_DATE
-                )
-                """
-            )
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS messages (
-                    author_name text,
-                    author_email text,
-                    message text,
-                    created_on date
-                )
-                """
-            )
-
-    def add_message(self, message: Message):
-        pass
-
-    def messages(self) -> list[Message]:
-        pass
-
-    def add_comment(self, comment: UnregisteredComment):
-        with self._connection.cursor() as cur:
-            cur.execute(
-                "INSERT INTO comments (post_slug, author_name, author_email, message, created_on) values (%s, %s, %s, %s, %s)",
-                (comment.post_slug, comment.author_name, comment.author_email, comment.message, comment.date),
-            )
-
-    def comments(self, post_slug: str) -> list[Comment]:
-        with self._connection.cursor() as cur:
-            cur.execute(
-                "SELECT id, post_slug, author_name, author_email, message, created_on FROM comments where post_slug = %s",
-                (post_slug,),
-            )
-            results = list(cur.fetchall())
-            return [
-                Comment(
-                    comment_id=r[0],
-                    post_slug=r[1],
-                    author_name=r[2],
-                    author_email=r[3],
-                    message=r[4],
-                    date=r[5],
-                )
-                for r in results
-            ]
 
 
 def test_adding_empty_comments():
@@ -92,7 +32,7 @@ def test_adding_and_retrieving_two_comments():
                 "Tom",
                 "t@tom.io",
                 "Salut !",
-                dt.date(2024, 12, 10),
+                date(2024, 12, 10),
             )
         )
         comments_repository.add_comment(
@@ -101,7 +41,7 @@ def test_adding_and_retrieving_two_comments():
                 "Mike",
                 "m@mike.io",
                 "Salut c'est Mike!",
-                dt.date(
+                date(
                     2024,
                     12,
                     11,
@@ -110,6 +50,14 @@ def test_adding_and_retrieving_two_comments():
         )
         retrieved = comments_repository.comments("dbz")
         assert retrieved == [
-            Comment(1, "dbz", "Tom", "t@tom.io", "Salut !", dt.date(2024, 12, 10)),
-            Comment(2, "dbz", "Mike", "m@mike.io", "Salut c'est Mike!", dt.date(2024, 12, 11)),
+            Comment(1, "dbz", "Tom", "t@tom.io", "Salut !", date(2024, 12, 10)),
+            Comment(2, "dbz", "Mike", "m@mike.io", "Salut c'est Mike!", date(2024, 12, 11)),
         ]
+
+
+def test_adding_and_retrieving_message():
+    with get_test_db_connection() as conn:
+        repo = PostgresCommentsRepository(conn)
+        message = Message("alex", "tests@petitapetit.io", "hey", datetime.now())
+        repo.add_message(message)
+        assert repo.messages() == [message]
